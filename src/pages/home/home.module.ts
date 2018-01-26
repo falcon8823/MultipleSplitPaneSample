c4d001945eb1ba4564cd2a566a2ceec731f14b1c
Hayato OKUMOTO <falcon@falconsrv.net>
2018-01-26 22:53:12 +0900

    Apply lazyloading

---
+++ src/pages/home/home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+
+import { HomePage } from './home';
+
+@NgModule({
+  imports: [
+    IonicPageModule.forChild(HomePage)
+  ],
+  exports: [],
+  declarations: [HomePage],
+  providers: [],
+})
+export class HomePageModule { }
